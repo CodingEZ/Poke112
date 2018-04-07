765bac2fcf6f27ddecd44e00a1dd1e9048cbc19c
nnagpal112 <38146360+nnagpal112@users.noreply.github.com>
2018-04-07 10:04:45 -0400

    Add files via upload

--- poke112.py
+++ poke112.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 pygame.init()
 
-win = pygame.display.set_mode((400,300))
 windowSize = (400,300)
+win = pygame.display.set_mode(windowSize)
+
 
 pygame.display.set_caption("First Game")
 
@@ -13,6 +15,7 @@ height = 60
 vel = 5
 
 WHITE = (255,255,255)
+GREEN = (0,255,0)
 WIDTH = HEIGHT = 100
 
 img = pygame.image.load('pikachu copy copy.jpg').convert()
@@ -56,6 +59,20 @@ class Student(pygame.sprite.Sprite):
         self.rect.center = (WIDTH/2, HEIGHT/2)
         self.x = 50
         self.y = 200
+        self.health = 0
+        self.attack = 0
+        self.time = 200
+        types = random.randint(0,2)
+        if type == 0:
+            self.type = 'grass'
+        elif type == 1:
+            self.type = 'water'
+        else:
+            self.type = 'fire'
+        self.x2 = 50
+        self.y2 = 265
+        self.width = 75
+        self.height = 2
     
     def handle_keys(self):
         key = pygame.key.get_pressed()
@@ -69,8 +86,29 @@ class Student(pygame.sprite.Sprite):
         elif key[pygame.K_LEFT]:
             self.x -= dist
     
+    def timerFired2(self,dt):
+        self.time += 1
+        if self.time%2 == 0:
+            self.x+=dt
+        else:
+            self.x -= dt
+    
+    def testRegrade(self,obj):
+        pass
+    
+    def health(self,surface):
+        surface = win
+        pygame.draw.rect(surface,GREEN,(self.x2,self.y2,width,height))
+    
+    def move(self):
+        self.x += 5
+    
+    def moveBack(self):
+        self.x -= 5
+    
     def draw(self,surface):
         surface = win
+        pygame.draw.rect(surface,GREEN,(self.x2,self.y2,self.width,self.height),5)
         surface.blit(self.image, (self.x,self.y))
 
 #win.blit(bg,(0,0))
@@ -88,7 +126,8 @@ while run:
     #all_sprites.update()
     #all_sprites.handle_keys()
     #all_sprites.draw(win)
-    
+    student.timerFired2(2)
+    #student.health(win)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
